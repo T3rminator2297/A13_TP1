42c2d8a9a284199adae98006da4cfae86c3ddfe8
T3rminator2297 <francis2297@hotmail.com>
2023-08-07 18:00:52 -0400

    Ajout des example de librairie

---
+++ src/ExamplePatternFactoryList/Example.java
@@ -0,0 +1,15 @@
+package ExamplePatternFactoryList;
+
+import java.util.Calendar;
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        /**
+         * La classe java.util.Calendar fournit un bon exemple d'utilisation du patron Factory Method.
+         */
+        // Utilise la fabrique pour créer une instance de calendrier appropriée
+        Calendar calendar = Calendar.getInstance();
+    }
+}
